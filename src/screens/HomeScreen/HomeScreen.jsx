36d66c4ca08c4984d670d79b468b6c9a068d6ae5
justyasen <ymanahov@gmail.com>
2022-01-06 16:43:02 +0200

    made the ratings and everything look good

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { View, Text, Image } from 'react-native';
-import { styles } from './styles'
+import { styles } from './styles';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'; 
 
 //Requiring image 
 var backShot = require ('../../assets/DSC05027.jpg');
 
 export const HomeScreen = () => {
-    return <View style={styles.title}>
-       <Image 
-       style={styles.image}
-       source={backShot}
+    return (
+      <View style={styles.root}>
+        <Image 
+          style={styles.image}
+          source={backShot}
        />
+      <View style={styles.textContainer}>
+        <Text style={styles.title} numberOfLines={3}> Audi RS7 </Text>
+        <View style={styles.rating}>
+          <FontAwesome name = 'star' size={18} color={'#E47911'}></FontAwesome>
+          <FontAwesome name = 'star' size={18} color={'#E47911'}></FontAwesome>
+          <FontAwesome name = 'star' size={18} color={'#E47911'}></FontAwesome>
+          <FontAwesome name = 'star' size={18} color={'#E47911'}></FontAwesome>
+          <FontAwesome name = 'star-half-full' size={18} color={'#E47911'}></FontAwesome> 
+        </View>
+        <Text style={styles.price}> $ 140 000  </Text> 
+        <Text style={styles.textDescription}> This V8 beast has 600 horsepower and can do 0-100 km/h in around 3 seconds! </Text>
+      </View>
+
      </View>
+     );
    };
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,11 +1,49 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+
     title: {
-        backgroundColor: '#8C92AC'
+        fontSize: 22,
+        marginTop: 10,
+        fontWeight: 'bold',
     },
+
     image: {
-        height: 650,
-        width: 400, 
+        flex: 2,
+        height: 350,
+        resizeMode: 'contain' //so it can resize depending on device size
     }, 
+
+    root: {
+        flexDirection: "row", 
+        margin: 5,
+        borderWidth: 1, 
+        borderColor: '#8C92AC',
+        borderRadius: 5,
+        backgroundColor: '#fff'
+    },
+
+    textContainer: {
+        margin: 3,
+        backgroundColor: '#fff',
+        flex: 1.8, 
+    }, 
+
+    textDescription: {
+        margin: 0,
+        backgroundColor: '#fff',
+        fontSize: 16, 
+    },
+
+    price: {
+        color: 'green',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+
+    rating: {
+        flexDirection: "row", 
+    }
+
+
 })
